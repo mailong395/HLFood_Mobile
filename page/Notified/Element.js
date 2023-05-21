f6e80ceb1e105266e7558879978aa3e0ee905fc0
MaiNhatHoangY2001 <hoan44pro@gmail.com>
2023-05-21 09:49:43 +0700

    Add socket and fix notifi status

--- page/Notified/Element.js
+++ page/Notified/Element.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { useContext } from 'react';
@@ -15,28 +15,28 @@ const Element = ({ data }) => {
   const dispatch = useDispatch();
   const [value, setValue] = useState({});
   const { getData, setGetData } = useContext(TableContext);
-  const userSelector = useSelector(state => state.auth);
+  const userSelector = useSelector((state) => state.auth);
   const axiosJWT = createAxios(userSelector?.data, dispatch, loginSuccess);
 
   const getNumberTable = (tables) => {
     if (tables) {
-      return tables.map(item => item.table_num).join();
+      return tables.map((item) => item.table_num).join();
     } else {
       return '0';
     }
-  }
+  };
 
-  const handleWatched = async () => {
+  const handleWatched = () => {
     if (!value.is_read) {
-      const params = {
-        ids: value.uuid
-      }
-      await updateStatusNotified(dispatch, params, userSelector?.data?.accessToken, axiosJWT);
+      const body = {
+        ids: `${value.uuid}`,
+      };
+      updateStatusNotified(dispatch, body, userSelector?.data?.accessToken, axiosJWT);
       setGetData(!getData);
     }
-  }
+  };
 
-  const LeftContent = props => <Avatar.Text {...props} label={value?.num_table} />
+  const LeftContent = (props) => <Avatar.Text {...props} label={value?.num_table} />;
 
   useEffect(() => {
     if (data) {
@@ -46,19 +46,19 @@ const Element = ({ data }) => {
         quantity_finished: data?.order_detail?.quantity_finished,
         num_table: getNumberTable(data?.order_detail?.order?.tables),
         is_read: data?.is_read,
-      }
+      };
       setValue(newData);
     }
-  }, [data])
+  }, [data]);
 
   return (
     <Card style={[styles.container, !value.is_read && styles.border]} onPress={handleWatched}>
       <Card.Title title={value?.name} subtitle={'Số lượng: ' + value?.quantity_finished} left={LeftContent} />
     </Card>
-  )
-}
+  );
+};
 
-export default Element
+export default Element;
 
 const styles = StyleSheet.create({
   container: {
@@ -68,5 +68,5 @@ const styles = StyleSheet.create({
   border: {
     borderWidth: 4,
     borderColor: 'green',
-  }
-})
+  },
+});
